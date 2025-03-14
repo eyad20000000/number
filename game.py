58f0799574efa1f7dfb726daa5f70a42f2233e35
eyad20000000 <eyadelsharawythebest@gmail.com>
2025-03-14 22:04:20 +0200

    hhhh

---
+++ game.py
@@ -0,0 +1,13 @@
+import random
+n = random.randint(1,100)
+guess = None
+while guess != n:
+    guess = int(input("Enter a number between 1 and 100: "))
+    if guess > n:
+        print("Too high!")
+    elif guess < n:
+        print("Too low!")
+    else:
+        print("You got it!")
+        break
+print("Game over!")
